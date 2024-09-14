0721226a659cde0346b292ab3fc5d6cd6b6e9243
David Bishop <davidjames1981@gmail.com>
2024-09-14 15:42:09 +0100

    first commit

---
+++ ITPortal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ITPortal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ITPortal.settings')
+
+application = get_asgi_application()
---
+++ ITPortal/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for ITPortal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('onboarding/', include('onboarding.urls')),  # Include the onboarding app URLs
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('favicon.ico', lambda x: HttpResponse(status=204)),
+    
+    # Redirect the root URL to the login page
+    path('', lambda request: redirect('login')),
+]

8da7321a42480eeca1a334726c1df0e4baf483b4
David Bishop (Laptop) <david.james.bishop@outlook.com>
2024-09-22 14:59:58 +0100

    change

--- ITPortal/settings.py
+++ ITPortal/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 
 database_url = os.environ.get("DATABASE_URL")
 
-DATABASES["default"] = database_url.parse(database_url)
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 #DATABASES["default"] = dj_database_url.parse("postgresql://db_test_django_user:IMmetgqm4Od55ezhxe8GBy8iptCHdkjZ@dpg-cro0ua08fa8c738m15o0-a.frankfurt-postgres.render.com/db_test_django")
  
